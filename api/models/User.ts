368fc0ba8f571dd8f23dc87ef10134186a237ba4
adzina <adzina3@vp.pl>
2017-09-25 15:24:57 +0200

    new passwords are encoded

--- api/models/User.ts
+++ api/models/User.ts
@@ -4,6 +4,8 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+ import * as bcrypt from "bcryptjs"
+
 
 module.exports = {
   tableName:"users",
@@ -12,13 +14,41 @@ module.exports = {
     last_name: {type:'string', required: true},
     email: {type: 'string', required: true, email: true, unique: true},
     password: {type: 'string', required:true},
-    role: {type: 'string', required:true}
-  }
-};
-/*
-export class User {
-	public id: number = 0;
-	public login: string = '';
-	public password:string = '';
+    role: {type: 'string', required:true},
+
+ },
+ /**
+  * this holds our validation message by
+  * sails-hook-validation dependency
+  */
+ validationMessages: { //hand for i18n & l10n
+   names: {
+     required: 'Name is required'
+   },
+   email: {
+     email: 'Provide valid email address',
+     required: 'Email is required',
+     unique: 'This email is already existing'
+   },
+   password: {
+     required: 'Password is required'
+   }
+ },
+
+ /**
+  * this is called so we can create our password hash for us
+  *
+  * before saving
+  * @param values
+  * @param cb
+  */
+ beforeCreate: function (values, cb) {
+
+   // Hash password
+   bcrypt.hash(values.password, 10, function (err, hash) {
+     if (err) return cb(err);
+     values.password = hash;
+     cb();
+   });
+ }
 }
-*/
